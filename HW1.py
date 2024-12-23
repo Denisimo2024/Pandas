0e7ad263f0555a3f97fc094c0630f73ad7ff36ef
Denisimo2024 <2132813@gmail.com>
2024-12-23 23:00:30 +0500

    finish

---
+++ HW1.py
@@ -0,0 +1,28 @@
+#Скачайте любой датасет с сайта https://www.kaggle.com/datasets
+
+# Загрузите набор данных из CSV-файла в DataFrame.
+#Выведите первые 5 строк данных, чтобы получить представление о структуре данных.
+#Выведите информацию о данных (.info()) и статистическое описание (.describe()).
+#2. Определите среднюю зарплату (Salary) по городу (City) - используйте файл приложенный к дз
+
+import pandas as pd
+
+df = pd.read_csv("Billionaires.csv")
+print("1)")
+print(df.head())
+
+print(df.info())
+
+print(df.describe())
+
+
+
+print("2)")
+
+df = pd.read_csv("dz.csv")
+
+print(df.head())
+
+group = df.groupby("City")["Salary"].mean()
+
+print(group)
